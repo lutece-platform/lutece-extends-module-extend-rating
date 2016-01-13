e3178fc62923a5c473bcdac38c7b90c63454ce0d
Jon Harper <jon.harper87@gmail.com>
2016-01-13 12:17:58 +0100

    EXTENDRAT-26: only use rating histories and extender in filters, forgot this one

--- src/java/fr/paris/lutece/plugins/extend/modules/rating/service/RatingService.java
+++ src/java/fr/paris/lutece/plugins/extend/modules/rating/service/RatingService.java
@@ -151,6 +151,7 @@ public class RatingService implements IRatingService
         ResourceExtenderHistoryFilter resourceExtenderHistoryFilter = new ResourceExtenderHistoryFilter(  );
         resourceExtenderHistoryFilter.setUserGuid( user.getName(  ) );
         resourceExtenderHistoryFilter.setIdExtendableResource( strIdExtendableResource );
+        resourceExtenderHistoryFilter.setExtendableResourceType( strExtendableResourceType );
         resourceExtenderHistoryFilter.setExtenderType( RatingResourceExtender.RESOURCE_EXTENDER );
         
         List<ResourceExtenderHistory> histories = _resourceExtenderHistoryService.findByFilter( resourceExtenderHistoryFilter );
