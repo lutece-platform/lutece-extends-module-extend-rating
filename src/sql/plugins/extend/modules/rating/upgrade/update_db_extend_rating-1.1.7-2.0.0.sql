a815f8290ab8eeecf37efdc131a030b64f19e586
HOHL <laurent.hohl@gmail.com>
2022-04-11 10:06:11 +0200

    EXTENDRAT-37: fix bug wrong iterator name

--- src/sql/plugins/extend/modules/rating/upgrade/update_db_extend_rating-1.1.7-2.0.0.sql
+++ src/sql/plugins/extend/modules/rating/upgrade/update_db_extend_rating-1.1.7-2.0.0.sql
@@ -1,7 +1,7 @@
 ALTER TABLE extend_rating MODIFY COLUMN id_rating int AUTO_INCREMENT;
 ALTER TABLE extend_rating_vote_history MODIFY COLUMN id_vote_history int AUTO_INCREMENT;
 
-ALTER TABLE  extend_rating_config MODIFY COLUMN id_vote_type VARCHAR(100) DEFAULT '' NOT NULL;
+ALTER TABLE extend_rating_config MODIFY COLUMN id_vote_type VARCHAR(100) DEFAULT '' NOT NULL;
 update extend_rating_config set id_vote_type='StarRating' where id_vote_type='1';
 update extend_rating_config set id_vote_type='ThumbRating' where id_vote_type='2';
 update extend_rating_config set id_vote_type='SimpleRating' where id_vote_type='3';
@@ -9,9 +9,4 @@ update extend_rating_config set id_vote_type='SimpleRating' where id_vote_type='
 DROP TABLE IF EXISTS extend_rating_vote_type;
 DROP TABLE IF EXISTS extend_rating_vote_history;
 
-
-
-
-
-
-alter table extend_rating_config modify column id_vote_type VARCHAR(100) DEFAULT '' NOT NULL;
+ALTER TABLE extend_rating_config MODIFY COLUMN id_vote_type VARCHAR(100) DEFAULT '' NOT NULL;
